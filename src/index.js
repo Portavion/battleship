4439d286376138465dc5d646a8bfabf49e52f45f
Portavion <156545430+Portavion@users.noreply.github.com>
2024-11-14 10:47:56 +0000

    refactor: placeShip function to be part of the player module

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import "./styles.css";
 import { Ship } from "./ship";
 import { Gameboard } from "./gameboard";
-import { Player } from "./player";
+import { Player, placeShip } from "./player";
 import { createGrid } from "./interface";
 
 const humanPlayer = new Player("human");
 const computer = new Player("computer");
 
-placeShip(humanPlayer);
-placeShip(computer);
-
-function placeShip(humanPlayer) {
-	humanPlayer.Gameboard.carrier.place(5, 10, 10, 10);
-	humanPlayer.Gameboard.battleship.place(7, 1, 10, 1);
-	humanPlayer.Gameboard.cruiser.place(4, 1, 4, 3);
-	humanPlayer.Gameboard.submarine.place(6, 3, 8, 3);
-	humanPlayer.Gameboard.destroyer.place(7, 6, 7, 7);
-}
+humanPlayer.placeShip();
+computer.placeShip();
 
 createGrid(humanPlayer);
 createGrid(computer);
--- src/player.js
+++ src/player.js
@@ -5,5 +5,12 @@ class Player {
 		this.Gameboard = new Gameboard();
 		this.playerType = playerStatus;
 	}
+	placeShip() {
+		this.Gameboard.carrier.place(5, 10, 10, 10);
+		this.Gameboard.battleship.place(7, 1, 10, 1);
+		this.Gameboard.cruiser.place(4, 1, 4, 3);
+		this.Gameboard.submarine.place(6, 3, 8, 3);
+		this.Gameboard.destroyer.place(7, 6, 7, 7);
+	}
 }
 export { Player };
